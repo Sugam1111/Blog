37d8b3bf769d6f7ec77dbe5c2a858762f4ae7818
Sugam Budhraja <sugam11nov@gmail.com>
2021-10-05 21:19:21 +1300

    Update prettier setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
     'next/core-web-vitals',
   ],
   rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
