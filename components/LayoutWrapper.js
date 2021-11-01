72a690d81f148b8df1f10a3f140f8cf35852bbbe
Sugam Budhraja <sugam11nov@gmail.com>
2021-11-02 12:06:06 +1300

    Update logo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -18,7 +18,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label="Tailwind CSS Blog">
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Logo className="w-20 h-20" />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
