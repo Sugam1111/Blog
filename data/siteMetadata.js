403476682b0790f6ded5c84c4c92cbeda7af054a
Sugam Budhraja <sugam11nov@gmail.com>
2022-04-01 16:11:08 +1300

    Giscus theme update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -30,23 +30,23 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: 'Sugam1111/Blog', //process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: 'MDEwOlJlcG9zaXRvcnk0MDAwOTQ3NjI', //process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: 'Announcements', //process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: 'DIC_kwDOF9j2Ks4COPdN', //process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'custom',
+      theme: 'light',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '/static/giscus.css',
+      themeURL: '', //'https://www.neuralco.de/css/giscus.css',
     },
     utterancesConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
